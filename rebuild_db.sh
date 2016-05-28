8a1b52974f7d029bba9f60a67897419793763188
Luis Dario Simonassi <ldsimonassi@gmail.com>
2016-05-28 16:15:27 -0300

    add seeds for addresses and users

--- rebuild_db.sh
+++ rebuild_db.sh
@@ -1,5 +1,12 @@
-rake db:drop
+# Clean an replace the DB.
 rm db/schema.rb
+cp db/development.sqlite3.with_cities_and_cars db/development.sqlite3
+
+# Run new migrations
 rake db:migrate
+
+# Seed new data into the DB
 rake db:seed
+
+# Open Database for exploration
 open ./db/development.sqlite3
